2fe7d11bc7a2a18f5b450dda12b2f8157ede6861
Carl Argabright <carl.argabright@gmail.com>
2020-07-12 18:09:26 -0700

    Mongoose Population

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -8,8 +8,10 @@ const campsiteRouter = express.Router();
 campsiteRouter.use(bodyParser.json());
 
 campsiteRouter.route('/')
+campsiteRouter.route('/')
   .get((req, res, next) => {
     Campsite.find()
+      .populate('comments.author')
       .then(campsites => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -44,6 +46,7 @@ campsiteRouter.route('/')
 campsiteRouter.route('/:campsiteId')
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then(campsite => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -79,6 +82,7 @@ campsiteRouter.route('/:campsiteId')
 campsiteRouter.route('/:campsiteId/comments')
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then(campsite => {
         if (campsite) {
           res.statusCode = 200;
@@ -96,6 +100,7 @@ campsiteRouter.route('/:campsiteId/comments')
     Campsite.findById(req.params.campsiteId)
       .then(campsite => {
         if (campsite) {
+          req.body.author = req.user._id;
           campsite.comments.push(req.body);
           campsite.save()
             .then(campsite => {
@@ -142,6 +147,7 @@ campsiteRouter.route('/:campsiteId/comments')
 campsiteRouter.route('/:campsiteId/comments/:commentId')
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+      .populate('comments.author')
       .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
           res.statusCode = 200;
